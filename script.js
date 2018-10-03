7e81edfd8cb84d2af0d92d27705aa50887580f91
Cristian Felipe <cristianpipe0494@gmail.com>
2018-10-03 14:54:16 -0600

    Agregar el Modal al repositorio. Agregar el background, y los contenedores.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 let modal = document.getElementById('modalSeccion');
 
 // declarando botón para abrir modal
-let abrir = document.getElementById("abrir");
+let abrir = document.getElementById("mainBtn");
 
 //elemento button que cierra el modal 
-let button = document.getElementsByClassName("cerrar")[0];
+let button = document.getElementById("closeBtn");
 
 // función de botón para abrir modal
 abrir.onclick = function() {
